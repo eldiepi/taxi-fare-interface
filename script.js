57d5d1f59287ced88055f624d1835a14cdf2335f
Pieter Dietrich <pieter@dietrich-fam.de>
2023-06-16 11:19:44 -0600

    updated

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoiZWxkaWVwaSIsImEiOiJjbGl5dGs2NjAwaTF6M2lyMTJ2ZGhobHhqIn0.qQL2c87lV2pwSWc6is3QKQ';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
